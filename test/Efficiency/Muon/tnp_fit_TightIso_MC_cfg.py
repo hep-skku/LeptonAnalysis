ece0324f5af832131b77bdb7e193455a42fa6274
Junghwan John Goh <jhgoh@cern.ch>
2016-06-07 18:54:10 +0200

    ID cut on probe for isolation efficiency measurement

--- test/Efficiency/Muon/tnp_fit_TightIso_MC_cfg.py
+++ test/Efficiency/Muon/tnp_fit_TightIso_MC_cfg.py
@@ -4,12 +4,13 @@ home = environ["HOME"]
 
 from CATTools.LeptonAnalysis.tnp_fit_muon_template_cfg import *
 process.tnpTightIso = process.tnpTemplate.clone()
-process.tnpTightIso.Efficiencies.TightIso = process.tnpTightIso.Efficiencies.TightId
 process.tnpTightIso.InputFileNames = cms.vstring(
      "file:"+home+"/eos/cms/store/group/phys_muon/TagAndProbe/76XtreeProduction/v41/TnPTree_76X_DYLL_M50_MadGraphMLM_part1.root",
      "file:"+home+"/eos/cms/store/group/phys_muon/TagAndProbe/76XtreeProduction/v41/TnPTree_76X_DYLL_M50_MadGraphMLM_part2.root",
 )
 process.tnpTightIso.OutputFileName = cms.string("tnp_fit_TightIso_MC.root")
+
+process.tnpTightIso.Efficiencies.TightIso = process.tnpTightIso.Efficiencies.TightId
 delattr(process.tnpTightIso.Efficiencies, 'TightId')
 process.tnpTightIso.Efficiencies.TightIso.EfficiencyCategoryAndState = cms.vstring("cut_TightIso", "below")
 #process.tnpTightIso.OutputFileName = cms.string("tnp_fit_TightIso03_RD.root")
@@ -19,6 +20,12 @@ process.tnpTightIso.Efficiencies.TightIso.EfficiencyCategoryAndState = cms.vstri
 ## Change PDF
 #process.tnpTightIso.Efficiencies.TightIso.BinToPDFmap = cms.vstring("voigtPlusCheb")
 ## Cut on denominator by adding BinnedVariables
+process.tnpTightIso.Variables.Tight2012 = cms.vstring("Tight2012 muon", "0", "1", "")
+process.tnpTightIso.Efficiencies.TightIso.BinnedVariables.Tight2012 = cms.vdouble(0.5,1.5) ## Tight2012==1
+process.tnpTightIso.Efficiencies.TightIso.BinnedVariables.dB = cms.vdouble(-0.2,0.2) ## abs(dB) < 0.2
+process.tnpTightIso.Efficiencies.TightIso.BinnedVariables.dzPV = cms.vdouble(-0.5,0.5) ## abs(dzPV) < 0.5
+delattr(process.tnpTightIso.Categories, "Tight2012") ## Should be removed to avoid duplicated definition
+
 process.tnpTightIso.Efficiencies.TightIso.BinnedVariables.pair_nJets30 = cms.vdouble(2,999) ## nJet >= 2 for ttbar
 
 process.p = cms.Path(process.tnpTightIso)
